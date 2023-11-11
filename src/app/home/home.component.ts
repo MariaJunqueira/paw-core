8600b67ae087761da72ab3abbf6b10c8ac0e528d
Maria Junqueira <mariaa.junqueira@gmail.com>
2023-11-11 12:02:28 +0100

    using we component decorator on the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,88 +1,33 @@
+import { Component } from '../stuff/decorators/webcomponent.decorator';
 import templateString from './home.component.html';
 import cssString from './home.component.scss';
-
-const SELECTOR = "home-component";
 
 const componentImports = {
   "about-component": () => import("../about/about.component"),
   // other components...
 };
 
+@Component({
+  selector: "home-component",
+  template: templateString,
+  styles: cssString,
+  components: componentImports,
+})
 export class HomeComponent extends HTMLElement {
-  static styleId = `_${SELECTOR}-${Date.now()}`;
-
   constructor() {
     super();
+    console.log("HomeComponent constructed!");
 
-    // Generate a unique identifier for this instance
-    this.setAttribute(HomeComponent.styleId, "");
-
-    // Prefix CSS selectors with the unique identifier
-    const scopedCssString = this.scopeCss(cssString);
-
-    // Apply the HTML and CSS
-    this.innerHTML = this.scopeHtml(templateString);
-    HomeComponent.appendScopedStyle(scopedCssString);
-    this.loadComponents(["about-component"]);
+    // Your component's constructor logic, if any
   }
-
-  // Scope the CSS by prefixing selectors with the unique identifier and excluding nested custom elements
-  scopeCss(css) {
-    return css.replace(
-      /([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)/g,
-      (match, selector) => {
-        // Scope each selector with the unique ID, excluding selectors for custom elements
-        const scopedSelector = selector
-          .split(",")
-          .map((part) => {
-            // Check if the selector is for a custom element (contains '-')
-            if (part.includes("-")) {
-              // If it's a custom element selector, leave it unmodified
-              return part.trim();
-            }
-            // Otherwise, apply the unique ID scoping
-            return `${part.trim()}[${HomeComponent.styleId}] `;
-          })
-          .join(", ");
-        return `${scopedSelector} ${match.endsWith(",") ? "," : " {"}`;
-      }
-    );
-  }
-
-  // Method to add the unique ID to all elements in the HTML string
-  scopeHtml(html) {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
-    // Iterate over all elements
-    doc.body.querySelectorAll("*").forEach((el) => {
-      // Check if the element is a custom element (has a hyphen in the tag name)
-      if (!el.tagName.includes("-")) {
-        // Apply the unique ID only to non-custom elements
-        el.setAttribute(HomeComponent.styleId, "");
-      }
-    });
-    return doc.body.innerHTML;
-  }
-
-  static appendScopedStyle(scopedCssString) {
-    if (!document.head.querySelector(`#${this.styleId}`)) {
-      const styleTag = document.createElement("style");
-      styleTag.id = this.styleId;
-      styleTag.textContent = scopedCssString;
-      document.head.appendChild(styleTag);
-    }
-  }
-
-  async loadComponents(componentNames) {
-    for (const name of componentNames) {
-      if (!customElements.get(name) && componentImports[name]) {
-        const module = await componentImports[name]();
-        customElements.define(name, module.default);
-      }
-    }
-  }
+  // Your component implementation...
 }
 
-if (!customElements.get(SELECTOR)) {
-  customElements.define(SELECTOR, HomeComponent);
-}
+// function WebComponent(options: {}): any | void {
+//   return class extends HTMLElement {
+//     fuel: number = 50;
+//     isEmpty(): boolean {
+//       return this.fuel == 0;
+//     }
+//   };
+// }
