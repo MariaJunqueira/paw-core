836b2755dc5f5d2e18c4b83378c6d1beae79d931
Maria Junqueira <mariaa.junqueira@gmail.com>
2023-11-13 00:16:50 +0100

    removing json tree viewer test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { container } from '../stuff/containers/dependency-container';
 import { Component } from '../stuff/decorators/webcomponent.decorator';
 import { HttpRequest } from '../stuff/services/http.request';
-import { JsonTreeViewer } from '../stuff/services/json.viewer';
 import templateString from './home.component.html';
 import cssString from './home.component.scss';
 
@@ -22,9 +21,8 @@ export default class HomeComponent extends HTMLElement {
     super();
     console.log("HomeComponent constructed!");
     const httpRequest: HttpRequest = container.resolve(HttpRequest);
-    const jsonTreeViewer: JsonTreeViewer = container.resolve(JsonTreeViewer);
     httpRequest.get("https://pokeapi.co/api/v2/pokemon/1").then((response) => {
-      this.response = jsonTreeViewer.render(response);
+      this.response = JSON.stringify(response);
     });
   }
 }
