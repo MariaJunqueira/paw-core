792edc198bc0e307e35e39ed19e4331c041bc2e0
Maria Junqueira <mariaa.junqueira@gmail.com>
2023-11-14 22:32:41 +0100

    refactoring component decorator

--- src/app/stuff/decorators/webcomponent.decorator.ts
+++ src/app/stuff/decorators/webcomponent.decorator.ts
@@ -1,12 +1,10 @@
 type BaseClass = HTMLElement;
-
 type Constructor = { new (...args: any[]): BaseClass };
 
 export function Component(options: any): any {
   return function <
     T extends Constructor & {
       observedAttributesSet?: Set<string>;
-      _lalaland?: string[];
     }
   >(constructor: T) {
     return class extends constructor {
@@ -15,6 +13,7 @@ export function Component(options: any): any {
         constructor.observedAttributesSet || new Set();
       private _isFullyConstructed = false;
       private _data;
+
       static get observedAttributes() {
         return Array.from(this.observedAttributesSet);
       }
@@ -22,8 +21,7 @@ export function Component(options: any): any {
       attributeChangedCallback(
         name: string,
         oldValue: string,
-        newValue: string,
-        isFirstChanged: boolean
+        newValue: string
       ) {
         // Defer execution until the component is fully constructed
         if (this._isFullyConstructed) {
@@ -31,92 +29,123 @@ export function Component(options: any): any {
             this[name] = newValue;
           }
 
-          this.dispatchEvent(
-            new CustomEvent("property-changed", {
-              detail: { property: name, oldValue, newValue, isFirstChanged },
-            })
-          );
+          this.dispatchPropertyChangedEvent(name, oldValue, newValue);
         }
       }
 
       constructor(...args: any) {
         super(...args);
-
-        const lalaland = (
-          this.constructor as typeof constructor & WebComponentClass
-        ).observedAttributesSet;
-        let maria: string[] = [];
-        if (lalaland?.size) {
-          maria = Array.from(lalaland);
-        }
-        maria = maria.concat(Object.getOwnPropertyNames(this));
-
-        this._data = {
-          // Initialize all properties here
-          // Example: title: '', description: '', etc.
-        };
-
-        this.addEventListener("property-changed", (e: CustomEvent) => {
-          // Update the specific property in your data object
-          this._data[e.detail.property] = e.detail.newValue;
-
-          // Call scopeHtml with the updated data object
-          this.innerHTML = this.scopeHtml(
-            options.template,
-            styleId,
-            this._data
-          );
-        });
-
-        maria?.forEach((property) => {
-          let value = this[property];
-          this._data[property] = value;
-          if (this[`_${property}`]?.isFirstChange) {
-            this[`_${property}`].isFirstChange = false;
-            this._data[property] = this[`_${property}`].value;
-          }
-
-          Object.defineProperty(this, property, {
-            get: () => value,
-            set: (newValue) => {
-              const oldValue = value;
-              value = newValue;
-
-              if (oldValue !== newValue) {
-                this.dispatchEvent(
-                  new CustomEvent("property-changed", {
-                    detail: {
-                      property,
-                      oldValue,
-                      newValue,
-                      isFirstChange: false,
-                    },
-                  })
-                );
-              }
-            },
-            enumerable: true,
-            configurable: true,
-          });
-        });
-
-        const styleId = (
-          this.constructor as typeof constructor & WebComponentClass
-        ).styleId;
-        this.setAttribute(styleId, "");
-        // Scope CSS and HTML
-        const scopedCssString = this.scopeCss(options.styles, styleId);
-
-        this.innerHTML = this.scopeHtml(options.template, styleId, this._data);
-
-        (
-          this.constructor as typeof constructor & WebComponentClass
-        ).appendScopedStyle(scopedCssString, styleId);
+        this.initializeData();
+        this.initializeStyles();
         this.loadDynamicComponents(options.components);
         this._isFullyConstructed = true;
       }
 
-      scopeCss(css: string, styleId: string): string {
+      private initializeData() {
+        const observedAttributes = (
+          this.constructor as typeof constructor & WebComponentClass
+        ).observedAttributesSet;
+        let properties = observedAttributes
+          ? Array.from(observedAttributes)
+          : [];
+        properties = properties.concat(Object.getOwnPropertyNames(this));
+
+        this._data = {}; // Initialize data as required
+
+        this.addEventListener(
+          "property-changed",
+          this.handlePropertyChange.bind(this)
+        );
+        properties.forEach((property) => this.initializeProperty(property));
+      }
+
+      private handlePropertyChange(e: CustomEvent) {
+        // Handle property change event
+        this._data[e.detail.property] = e.detail.newValue;
+        this.innerHTML = this.scopeHtml(
+          options.template,
+          (this.constructor as typeof constructor & WebComponentClass).styleId,
+          this._data
+        );
+      }
+
+      private initializeProperty(property: string) {
+        let value = this[property];
+        this._data[property] = value;
+
+        this.handleSpecialInitialization(property);
+        this.definePropertyGettersAndSetters(property);
+      }
+
+      private handleSpecialInitialization(property: string) {
+        if (this[`_${property}`]?.isFirstChange) {
+          this[`_${property}`].isFirstChange = false;
+          this._data[property] = this[`_${property}`].value;
+        }
+      }
+
+      private definePropertyGettersAndSetters(property: string) {
+        let value = this[property];
+        Object.defineProperty(this, property, {
+          get: () => value,
+          set: (newValue) => {
+            const oldValue = value;
+            value = newValue;
+
+            if (oldValue !== newValue) {
+              this.emitPropertyChangedEvent(property, oldValue, newValue);
+            }
+          },
+          enumerable: true,
+          configurable: true,
+        });
+      }
+
+      private emitPropertyChangedEvent(
+        property: string,
+        oldValue: any,
+        newValue: any
+      ) {
+        this.dispatchEvent(
+          new CustomEvent("property-changed", {
+            detail: {
+              property,
+              oldValue,
+              newValue,
+            },
+          })
+        );
+      }
+
+      private initializeStyles() {
+        const styleId = (
+          this.constructor as typeof constructor & WebComponentClass
+        ).styleId;
+        this.setAttribute(styleId, "");
+        const scopedCssString = this.scopeCss(options.styles, styleId);
+        this.innerHTML = this.scopeHtml(options.template, styleId, this._data);
+        (
+          this.constructor as typeof constructor & WebComponentClass
+        ).appendScopedStyle(scopedCssString, styleId);
+      }
+
+      private dispatchPropertyChangedEvent(
+        name: string,
+        oldValue: string,
+        newValue: string
+      ) {
+        this.dispatchEvent(
+          new CustomEvent("property-changed", {
+            detail: {
+              name,
+              oldValue,
+              newValue,
+            },
+          })
+        );
+      }
+
+      private scopeCss(css: string, styleId: string): string {
         return css.replace(
           /([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)/g,
           (match, selector) => {
@@ -134,7 +163,7 @@ export function Component(options: any): any {
         );
       }
 
-      scopeHtml(html: string, styleId: string, data: any = {}): string {
+      private scopeHtml(html: string, styleId: string, data: any = {}): string {
         const templatedHtml = html.replace(/\{\{\s*(\w+)\s*\}\}/g, (_, key) => {
           return data[key];
         });
@@ -150,7 +179,10 @@ export function Component(options: any): any {
         return doc.body.innerHTML;
       }
 
-      static appendScopedStyle(scopedCssString: string, styleId: string) {
+      private static appendScopedStyle(
+        scopedCssString: string,
+        styleId: string
+      ) {
         if (!document.head.querySelector(`#${styleId}`)) {
           const styleTag = document.createElement("style");
           styleTag.id = styleId;
@@ -159,7 +191,9 @@ export function Component(options: any): any {
         }
       }
 
-      loadDynamicComponents(components: Record<string, () => Promise<any>>) {
+      private loadDynamicComponents(
+        components: Record<string, () => Promise<any>>
+      ) {
         if (!components) return;
         for (const [name, importFn] of Object.entries(components)) {
           if (!customElements.get(name)) {
@@ -178,8 +212,8 @@ export function Component(options: any): any {
     } as unknown as T;
   };
 }
+
 interface WebComponentClass {
   appendScopedStyle: (scopedCssString: string, styleId: string) => void;
-  styleId;
-  string;
+  styleId: string;
 }
